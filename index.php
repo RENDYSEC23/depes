e2b95041cd7bc8f60a6e6da399e1b23935e84446
RENDYSEC23 <rendysetiawansec@gmail.com>
2024-12-10 22:42:09 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,502 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head> 
+  <link rel="icon" href="https://h.top4top.io/p_3154cdvq20.gif" type="image/x-icon"> 
+  <meta name="keywords" content="Touch By FANN">
+  <meta name="description" content="FANN">
+  <meta name="author" content="FANN Was Here">
+  <meta name="theme-color" content="#343a40"/>
+  <title>Touch By FANN</title> 
+  <link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet" type="text/css"> 
+  <link rel="stylesheet" href="https://rawcdn.githack.com/nako48/shiraookaaaa/f37b3bb82ba199ac8df3a394d8652e56872935a9/style-css.css"> <div class="glitch-wrapper">
+  <script type="text/javascript" src="https://drvee07.github.io/loc/indo.jt.js"></script>
+
+            <script type="text/javascript"> drvee.init("https://drvee07.github.io/loc/jet.png"); </script>
+  <style>
+ body {
+        color: #FFFFFF;
+        font-family: "Kelly Slab", sans-serif; 
+        font-weight: 60; 
+        height: 80vh;
+        margin: 0; 
+        background-color: black;
+}
+.hmm{
+        height: 67vh; 
+}
+.ya {
+        align-items: center;
+        display: flex;
+        justify-content: center; 
+}
+.udah{
+        position: relative; 
+}
+.iya {
+        text-align: center; 
+}
+</style>
+</head>
+ <body> 
+    <div class="Low Security Website"> 
+     <div class="iya"> 
+       <img src="https://h.top4top.io/p_3154cdvq20.gif" alt="" width="200" height="200"><br>
+       	<style type="text/css">
+        body {
+            background-color: black;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            color: #333;
+            font-size: 50px;
+            margin: 0 auto;
+            text-align: center;
+            text-transform: uppercase;
+            font-family: Orbitron;
+        }
+        .neon {
+            color: #FFFFFF;
+            text-shadow: 0 0 5px #FF0000, 0 0 10px #FF0000, 0 0 30px #FF0000, 0 0 45px #FF0000, 0 0 60px #FF0000;
+        }
+        .matrix {
+            color: #FFFFFF;
+            font-family: Arial, Courier, Monotype;
+            font-size: 15pt;
+            text-align: center;
+            padding: 0px;
+            margin: 0px;
+        }
+        .jokitz1 {
+            text-align: center;
+	}
+.jokitz2{
+	text-align : center;
+	font-family : Courier;
+	}
+</style>
+<script type="text/javascript">
+
+<!--
+
+var message="Ooooooppppssssss....! Sorry";
+
+///////////////////////////////////
+
+function clickIE() {if (document.all) {(message);return false;}}
+
+function clickNS(e) {if
+
+(document.layers||(document.getElementById&&!document.all)) {
+
+if (e.which==2||e.which==3) {(message);return false;}}}
+
+if (document.layers)
+
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+document.oncontextmenu=new Function("return false")
+
+// -->
+
+</script>
+
+<script type="text/javascript" language="javascript">
+
+
+<!--
+
+var rows=1; // must be an odd number
+
+var speed=10; // lower is faster
+
+var reveal=2; // between 0 and 2 only. The higher, the faster the word appears
+
+var effectalign="center" //enter "center" to center it.
+
+var w3c=document.getElementById && !window.opera;;
+
+var ie45=document.all && !window.opera;
+
+var ma_tab, matemp, ma_bod, ma_row, x, y, columns, ma_txt, ma_cho;
+
+var m_coch=new Array();
+
+var m_copo=new Array();
+
+window.onload=function() {
+
+	if (!w3c && !ie45) return
+
+  var matrix=(w3c)?document.getElementById("matrix"):document.all["matrix"];
+
+  ma_txt=(w3c)?matrix.firstChild.nodeValue:matrix.innerHTML;
+
+  ma_txt=" "+ma_txt+" ";
+
+  columns=ma_txt.length;
+
+  if (w3c) {
+
+    while (matrix.childNodes.length) matrix.removeChild(matrix.childNodes[0]);
+
+    ma_tab=document.createElement("table");
+
+    ma_tab.setAttribute("border", 0);
+
+    ma_tab.setAttribute("align", effectalign);
+
+    ma_tab.style.backgroundColor="#000000";
+
+    ma_bod=document.createElement("tbody");
+
+    for (x=0; x<rows; x++) {
+
+      ma_row=document.createElement("tr");
+
+      for (y=0; y<columns; y++) {
+
+        matemp=document.createElement("td");
+
+        matemp.setAttribute("id", "Mx"+x+"y"+y);
+
+        matemp.className="matrix";
+
+        matemp.appendChild(document.createTextNode(String.fromCharCode(160)));
+
+        ma_row.appendChild(matemp);
+
+      }
+
+      ma_bod.appendChild(ma_row);
+
+    }
+
+    ma_tab.appendChild(ma_bod);
+
+    matrix.appendChild(ma_tab);
+
+  } else {
+
+    ma_tab='<ta'+'ble align="'+effectalign+'" border="0" style="background-color:#000000">';
+
+    for (var x=0; x<rows; x++) {
+
+      ma_tab+='<t'+'r>';
+
+      for (var y=0; y<columns; y++) {
+
+        ma_tab+='<t'+'d class="matrix" id="Mx'+x+'y'+y+'"> </'+'td>';
+
+      }
+
+      ma_tab+='</'+'tr>';
+
+    }
+
+    ma_tab+='</'+'table>';
+
+    matrix.innerHTML=ma_tab;
+
+  }
+
+  ma_cho=ma_txt;
+
+  for (x=0; x<columns; x++) {
+
+    ma_cho+=String.fromCharCode(32+Math.floor(Math.random()*94));
+
+    m_copo[x]=0;
+
+  }
+
+  ma_bod=setInterval("mytricks()", speed);
+
+}
+
+
+
+function mytricks() {
+
+  x=0;
+
+  for (y=0; y<columns; y++) {
+
+    x=x+(m_copo[y]==100);
+
+    ma_row=m_copo[y]%100;
+
+    if (ma_row && m_copo[y]<100) {
+
+      if (ma_row<rows+1) {
+
+        if (w3c) {
+
+          matemp=document.getElementById("Mx"+(ma_row-1)+"y"+y);
+
+          matemp.firstChild.nodeValue=m_coch[y];
+
+        }
+
+        else {
+
+          matemp=document.all["Mx"+(ma_row-1)+"y"+y];
+
+          matemp.innerHTML=m_coch[y];
+
+        }
+
+        matemp.style.color="#FF0000";
+
+        matemp.style.fontWeight="bold";
+
+      }
+
+      if (ma_row>1 && ma_row<rows+2) {
+
+        matemp=(w3c)?document.getElementById("Mx"+(ma_row-2)+"y"+y):document.all["Mx"+(ma_row-2)+"y"+y];
+
+        matemp.style.fontWeight="normal";
+
+        matemp.style.color="#FF0000";
+
+      }
+
+      if (ma_row>2) {
+
+          matemp=(w3c)?document.getElementById("Mx"+(ma_row-3)+"y"+y):document.all["Mx"+(ma_row-3)+"y"+y];
+
+        matemp.style.color="#FF0000";
+
+      }
+
+      if (ma_row<Math.floor(rows/2)+1) m_copo[y]++;
+
+      else if (ma_row==Math.floor(rows/2)+1 && m_coch[y]==ma_txt.charAt(y)) zoomer(y);
+
+      else if (ma_row<rows+2) m_copo[y]++;
+
+      else if (m_copo[y]<100) m_copo[y]=0;
+
+    }
+
+    else if (Math.random()>0.9 && m_copo[y]<100) {
+
+      m_coch[y]=ma_cho.charAt(Math.floor(Math.random()*ma_cho.length));
+
+      m_copo[y]++;
+
+    }
+
+  }
+
+  if (x==columns) clearInterval(ma_bod);
+
+}
+
+
+
+function zoomer(ycol) {
+
+  var mtmp, mtem, ytmp;
+
+  if (m_copo[ycol]==Math.floor(rows/2)+1) {
+
+    for (ytmp=0; ytmp<rows; ytmp++) {
+
+      if (w3c) {
+
+        mtmp=document.getElementById("Mx"+ytmp+"y"+ycol);
+
+        mtmp.firstChild.nodeValue=m_coch[ycol];
+
+      }
+
+      else {
+
+        mtmp=document.all["Mx"+ytmp+"y"+ycol];
+
+        mtmp.innerHTML=m_coch[ycol];
+
+      }
+
+      mtmp.style.color="#FF0000";
+
+      mtmp.style.fontWeight="bold";
+
+    }
+
+    if (Math.random()<reveal) {
+
+      mtmp=ma_cho.indexOf(ma_txt.charAt(ycol));
+
+      ma_cho=ma_cho.substring(0, mtmp)+ma_cho.substring(mtmp+1, ma_cho.length);
+
+    }
+
+    if (Math.random()<reveal-1) ma_cho=ma_cho.substring(0, ma_cho.length-1);
+
+    m_copo[ycol]+=199;
+
+    setTimeout("zoomer("+ycol+")", speed);
+
+  }
+
+  else if (m_copo[ycol]>200) {
+
+    if (w3c) {
+
+      mtmp=document.getElementById("Mx"+(m_copo[ycol]-201)+"y"+ycol);
+
+      mtem=document.getElementById("Mx"+(200+rows-m_copo[ycol]--)+"y"+ycol);
+
+    }
+
+    else {
+
+      mtmp=document.all["Mx"+(m_copo[ycol]-201)+"y"+ycol];
+
+      mtem=document.all["Mx"+(200+rows-m_copo[ycol]--)+"y"+ycol];
+
+    }
+
+    mtmp.style.fontWeight="normal";
+
+    mtem.style.fontWeight="normal";
+
+    setTimeout("zoomer("+ycol+")", speed);
+
+  }
+
+  else if (m_copo[ycol]==200) m_copo[ycol]=100+Math.floor(rows/2);
+
+  if (m_copo[ycol]>100 && m_copo[ycol]<200) {
+
+    if (w3c) {
+
+      mtmp=document.getElementById("Mx"+(m_copo[ycol]-101)+"y"+ycol);
+
+      mtmp.firstChild.nodeValue=String.fromCharCode(160);
+
+      mtem=document.getElementById("Mx"+(100+rows-m_copo[ycol]--)+"y"+ycol);
+
+      mtem.firstChild.nodeValue=String.fromCharCode(160);
+
+    }
+
+    else {
+
+      mtmp=document.all["Mx"+(m_copo[ycol]-101)+"y"+ycol];
+
+      mtmp.innerHTML=String.fromCharCode(160);
+
+      mtem=document.all["Mx"+(100+rows-m_copo[ycol]--)+"y"+ycol];
+
+      mtem.innerHTML=String.fromCharCode(160);
+
+    }
+
+    setTimeout("zoomer("+ycol+")", speed);
+
+  }
+
+  
+
+  //start
+
+var h1 = document.getElementsByTagName("h1")[0],
+
+text = h1.innerText || h1.textContent,
+
+split = [], i, lit = 0, timer = null;
+
+for(i = 0; i < text.length; ++i) {
+
+split.push("<span>" + text[i] + "</span>");
+
+}
+
+h1.innerHTML = split.join("");
+
+split = h1.childNodes;
+
+
+
+var flicker = function() {
+
+lit += 0.01;
+
+if(lit >= 1) {
+
+clearInterval(timer);
+
+}
+
+for(i = 0; i < split.length; ++i) {
+
+if(Math.random() < lit) {
+
+split[i].className= "neon";
+
+} else {
+
+split[i].className = "";
+
+}
+
+}
+
+}
+
+setInterval(flicker, 100);
+
+
+}
+
+
+</script>
+</head>
+
+<center>
+<h1>WE ARE DEFACER INDONESIA</h1>
+
+<div id="matrix" class="auto-style8">____________________________________________________________________</div><br>
+       <div class="glitch" data-text="Low Security">
+       <font color="white" face="5"><font color="red">Touch By<font color="white"> FANN</font></font></font></font></font>
+       </div><br><br>
+       	<font color="white" face="5" font size="5"><font color="gold"><br>
+SELAMA KEMERDEKAAN BANGSA PALESTINA BELUM DISERAHKAN KEPADA ORANG-ORANG PALESTINA,<br>
+MAKA SELAMA ITU JUGA BANGSA INDONESIA BERDIRI MENANTANG PENJAJAHAN ISREAL<br></font><font color="white" font size="3"><br>CLICK TO PLAY MUSIC<br></font></font>
+		
+    <div class="flex-center position-ref full-height">
+
+
+      <div class="content">
+        <div class="text">
+              <source src="https://f.top4top.io/m_3169ife6u0.mp3" type="audio/mp3">
+
+</audio> 
+</div>
+  <font size="">
+  <p>
+  <audio src="https://f.top4top.io/m_3169ife6u0.mp3" autoplay controls></audio>
+</p>
+<div class="footer-greetings"> 
+  <audio autoplay>
+  </audio>
+    
+
+  </script>
+
+</br>
+
+</p>
+
+</i></a>
+      </div>
+   </div>
+</body>
+</html>
+
